0c2bf887f7e8aa66a5da447c16bdb2818ab7c73e
Admin <Admin@DESKTOP-52351T8>
2024-08-05 12:42:11 +0900

    first commit

---
+++ jvp0614/src/jvp0614/June14.java
@@ -0,0 +1,12 @@
+package jvp0614;
+
+public class June14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1 = 'A' + 1;
+		ch1 = "A".charAt(0);
+		System.out.println("ch1="+ch1);
+	}
+
+}
