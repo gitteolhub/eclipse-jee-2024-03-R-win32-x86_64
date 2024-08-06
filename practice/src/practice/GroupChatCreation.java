17faf334ef65ca8eb54604e8c7c9f16bb5cd8f14
Admin <Admin@DESKTOP-52351T8>
2024-08-06 17:17:47 +0900

    주석 설명 여기저기 추가

--- practice/src/practice/GroupChatCreation.java
+++ practice/src/practice/GroupChatCreation.java
@@ -19,7 +19,7 @@ public class GroupChatCreation {
                 System.out.println("A가 조원" + (i + 1) + "의 번호를 확인한다.");
                 System.out.print("번호가 없습니다. 번호를 입력해주세요:");
                 
-             // 번호 입력 받음.
+             // 번호 입력 받음. 값 입력 없이 enter를 치면 줄바꿈만 되는데 nextLine()을 사용하지 않고 해결하는법 찾아야함.
                 while (true) {
                     try {
                         // 입력받은 값을 숫자 형태로 변환
@@ -30,7 +30,7 @@ public class GroupChatCreation {
                         break;
                     } catch (InputMismatchException e) {
                         // 변환 실패, 숫자 형태의 번호가 아님
-                        System.err.println("숫자만 입력해주세요:");
+                        System.err.print("숫자만 입력해주세요:");
                         // 잘못된 입력을 버리고 다시 입력받기 위해 scanner.next()를 호출. break;를 적지 않았기 때문에 다시 while문이 실행된다.
                         scanner.next();
                     }
@@ -38,10 +38,11 @@ public class GroupChatCreation {
             }
         }
         
-        // 모든 조원의 번호가 있는지 최종 확인
+        //올바른 숫자를 입력하지 않으면 while문을 빠져나갈수 없게 수정했기 때문에 필요없는 부분이지만 if (number == null || number.isEmpty())을 사용해 번호 점검을 하는 방식을 배웠기 때문에 남겨둠.
+        // 모든 조원의 번호가 있는지 최종 확인.
         boolean allNumbersPresent = true;
         for (String number : memberNumbers) {
-            if (number == null || number.isEmpty()) {
+            if (number == null || number.isEmpty()) {  //null은 값이 입력되지 않음, number.isEmpty()는 빈 값이 입력됐음을 뜻함.
                 allNumbersPresent = false;
                 break;
             }
@@ -51,7 +52,7 @@ public class GroupChatCreation {
         if (allNumbersPresent) {
             System.out.println("모든 조원의 번호가 확인되었습니다. 그룹 채팅방을 생성합니다.");
         } else {
-            System.out.println("모든 조원의 번호가 확인되지 않았습니다. 다시 번호를 확인하세요.");
+            System.err.println("모든 조원의 번호가 확인되지 않았습니다. 다시 번호를 확인하세요.");
         }
         
         scanner.close();
