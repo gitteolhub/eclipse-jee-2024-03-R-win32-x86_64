492753196c848bfee402c5150dbb9f0665f94e95
Admin <Admin@DESKTOP-52351T8>
2024-08-14 12:17:40 +0900

    이것이 자바다 확인문제 5단원 7번 풀기

--- practice/src/thisIsJava/Q5_7.java
+++ practice/src/thisIsJava/Q5_7.java
@@ -1,12 +1,11 @@
 package thisIsJava;
 
 public class Q5_7 {
-	
 	public static void main(String[] args) {
-		int[] array = {1, 5, 3, 8, 2};
+		int[] array = { 1, 5, 3, 8, 2 };
 		int max = array[0];
 		for (int arr : array) {
-			if(arr > max) {
+			if (arr > max) {
 				max = arr;
 			}
 		}
