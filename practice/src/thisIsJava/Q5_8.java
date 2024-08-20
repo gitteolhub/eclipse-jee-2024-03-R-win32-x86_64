8d4435b10805b7dbc3d6d3ef31cfed0f62ecf08f
Admin <Admin@DESKTOP-52351T8>
2024-08-20 17:03:26 +0900

    이것이 자바다 5단원 확인문제 8번

---
+++ practice/src/thisIsJava/Q5_8.java
@@ -0,0 +1,23 @@
+package thisIsJava;
+
+public class Q5_8 {
+	public static void main(String[] args) {
+		int[][] array = {
+				{95, 86},
+				{83, 92, 96},
+				{78, 83, 93, 87, 88}
+		};
+		int sum = 0;
+		double avg = 0;
+		int count = 0;
+		for (int[] arr : array) {
+			for (int value : arr) {
+				sum += value;
+				count += 1;
+			}
+		}
+		avg = (double) sum / count;
+		System.out.println(sum);
+		System.out.println(avg);
+	}
+}
