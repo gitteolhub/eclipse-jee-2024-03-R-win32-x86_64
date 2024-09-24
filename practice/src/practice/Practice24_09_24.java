5895e1392f5c0d8ffa6e964bd88a30087f9f4fcf
gitteolhub <ht2001@naver.com>
2024-09-24 16:03:00 +0900

    print 출력 형식 확인

---
+++ practice/src/practice/Practice24_09_24.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice24_09_24 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 1; i <= T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("Case #" + i + ": " + A+B);
+        }
+        sc.close();
+	}
+
+}
