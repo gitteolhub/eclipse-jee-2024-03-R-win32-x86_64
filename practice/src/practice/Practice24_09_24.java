b647cb9e382037380e71f5d6c086a84f39862d04
gitteolhub <ht2001@naver.com>
2024-09-24 16:22:42 +0900

    이클립스 콘솔창 작동 확인

--- practice/src/practice/Practice24_09_24.java
+++ practice/src/practice/Practice24_09_24.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Practice24_09_24 {
 
 	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
 		Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int i = 1; i <= T; i++) {
